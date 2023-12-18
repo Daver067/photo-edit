3e11707904e78a07fbfcfde0730d327c46fb407b
Dave Diemert <davediemert_88@hotmail.com>
2023-12-18 18:57:10 +0700

    takes photos from photosIn, adds a border and saves them in photosOut

--- photo-edit.py
+++ photo-edit.py
@@ -3,19 +3,35 @@ import os
 import json
 
 def main():
-
-    im = Image.open('pyramids.jpeg')
-    print(im.format, im.size, im.mode)
-    coord = (0,0)
-    rgbValue = (255,255,255)
-    for i in range(im.size[0]):
-        for j in range(im.size[1]):
-            if j < 2 or j > 955 or i < 3 or i > 1276:
-                coord = (i,j)
-                coord2 = (im.size[0] - 1 - i, im.size[1] - 1 - j)
-                im.putpixel(coord, rgbValue)
-                im.putpixel(coord2, rgbValue)
-    im.save('pyramids2.jpeg')
+    pathIn = './photosIn/'
+    photoList = dict()
+    pathOut = './photosOut/'
+    count = 0
+    for filename in os.listdir(pathIn):
+            if filename == ".DS_Store":
+                 continue
+            photoList[str(count)] = filename
+            count += 1
+    for i in range(len(photoList)):
+            currentPath = f"{pathIn}{photoList[str(i)]}"
+            savePath = f"{pathOut}{photoList[str(i)]}"
+            im = Image.open(currentPath)
+            rgbValue = (255,255,255,1)
+            for i in range(im.size[0]):
+                for j in range(3):
+                        coord = (i,j)
+                        coord2 = (im.size[0] - 1 - i, im.size[1] - 1 - j)
+                        im.putpixel(coord, rgbValue)
+                        im.putpixel(coord2, rgbValue)
+            for i in range(im.size[1]):
+                for j in range(3):
+                    coord = (j,i)
+                    coord2 = (im.size[0] - 1 - j, im.size[1] - 1 - i)
+                    im.putpixel(coord, rgbValue)
+                    im.putpixel(coord2, rgbValue)
+            im.save(savePath)
     return
+
+
 
 main()
